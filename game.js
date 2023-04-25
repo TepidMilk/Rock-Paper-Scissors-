1978fd5c74a84885816d7702f28016318449b631
Noah Kingrey <ivorykingrey@gmail.com>
2023-04-25 12:43:08 -0500

    add buttons to index and restart script
    The code for the script was deleted to start from scratch

--- game.js
+++ game.js
@@ -5,97 +5,32 @@ function getComputerChoice(){
     
     //assigns computerChoice R, P or S based off of its previous value
     if (computerChoice == 1){
-        return 'Rock'
+        return 'rock'
     }
     else if (computerChoice == 2){
-        return 'Paper';
+        return 'paper';
     } 
     else {
-        return 'Scissors';
+        return 'scissors';
     }
 }
 
-//Plays a round of RPS between the user and the computer
-function playRound(){
-    //prompts user for their action (case insensitive)
-    let player = prompt('Rock, Paper, or Scissors?')
-    player = player.toLowerCase();
-    player = player.substring(0,1).toUpperCase() + player.substring(1)
+const buttons = document.querySelectorAll('button');
 
-    //changes computer selection each round
+// Plays a round of RPS between the user and the computer
+function playRound(playerChoice){
+
     let computerSelection = getComputerChoice();
-    
-    //checks if user selected from allowed options and reprompts if not
-    while (player != "Rock" && player != "Paper" && player != "Scissors"){
-        alert('Please pick from Rock, Paper, or Scissors.')
-        player = prompt('Rock, Paper, or Scissors?')
-        player = player.toLowerCase();
-        player = player.substring(0,1).toUpperCase() + player.substring(1);
-    }
 
-    //checks for draw
-    if (player == computerSelection){
-        console.log('Draw!');
-        return; 
-    }
-
-    //returns win or lose from user rock selection
-    while(player == 'Rock'){
-        if (computerSelection == 'Paper'){
-            console.log('You Lose! Paper covers Rock!');
-            return "Lose";
-        } else console.log('You Win! Rock crushes Scissors!')
-        return 'Win'; 
-    }
-
-    //returns win or lose from user paper selection
-    while (player == 'Paper'){
-        if (computerSelection == 'Rock'){
-            console.log('You Win! Paper covers Rock!');
-            return 'Win';
-        } else console.log('You Lose! Scissors cut Paper!');
-        return "Lose";
-    }
-
-    //returns win or lose from user scissors selection
-    while (player == 'Scissors'){
-        if (computerSelection == 'Rock'){
-            console.log('You Lose! Rock crushes Scissors!')
-            return "Lose";
-        } else console.log('You Win! Scissors cut Paper!');
-        return 'Win';
-    }
 }
 
-//plays a best of 5 game of RPS
-function game(){
-    //keeps track of score
-    let score = 0
-
-    console.log("Let's play Rock Paper Scissors! 5 rounds")
-
-    //checks for best of 5
-    while (score < 3 && score > -3){
-        //plays RPS until there is a winner
-        for (i = 1; i <= 5; i++){
-                let result = playRound();
-                if (result == 'Win'){
-                    score++
-                }
-                else if (result == 'Lose'){
-                    score--
-                }
-        }
-    }
-    
-    //determines winner based off score
-    if (score > 0){
-        console.log('Winner Winner Chicken Dinner!');
-        return;
-    }
-    else if (score < 0){
-        console.log('Loser Loser Grain Smoothie');
-        return;
-    } else console.log("Seems we're evenly matched")
-    return;
+function winCheck(player, computer){
+    const rockWin = 'rock scissors'
+    const paperWin = 'paper rock'
+    const scissorWin = 'scissors paper'
+    let check = `${player} ${computer}`
+    if (check == rockWin || check == paperWin || check == scissorWin){
+        return 'win'
+    } else return 'lose'
 }
+
